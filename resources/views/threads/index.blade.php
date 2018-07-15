bdcf0a35da350388a214dfb6d0d03997af57e7ca
bulveyz <rus131354@icloud.com>
2018-07-15 22:09:06 +0300

    a

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
     @foreach($threads as $thread)
-        <a href="/threads/{{$thread->channel->name}}/{{$thread->id}}">{{$thread->title}}</a>
-        <hr>
+        <div class="thread bg-white rounded p-3 text-left">
+            <div class="thread__header mb-5">
+                <h3>{{$thread->title}}</h3>
+            </div>
+            <div class="thread__body mb-5 text-truncate" style="max-width: 100%;">
+                @if($thread->IsPublished)
+                    {!! $thread->body !!}
+                @endif
+            </div>
+            <div class="thread__footer d-flex justify-content-between">
+                <p>{{$thread->created_at->diffForHumans()}}</p>
+                <a href="/threads/{{$thread->channel->name}}/{{$thread->id}}"
+                   class="btn btn-outline-success">Читать далее</a>
+            </div>
+        </div>
     @endforeach
 @endsection
