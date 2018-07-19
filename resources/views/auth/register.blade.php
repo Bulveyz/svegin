69a44f0ace30a3dab53cb061e3a35fea1c5a5d72
htbit <karlasXXXS@gmail.com>
2018-07-19 16:22:48 +0600

    обновил дизайн регистрации

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="intro neon blue" style="padding-top: 0px; padding-bottom: 0px;">
                 <div class="card-header">{{ __('Регистрация') }}</div>
 
                 <div class="card-body">
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Ник') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }} neon blue" name="name" value="{{ old('name') }}" required autofocus style="background-color: #0d0121; color: white;">
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail адрес') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} neon blue" name="email" value="{{ old('email') }}" required style="background-color: #0d0121; color: white;">
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +43,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} neon blue" name="password" required style="background-color: #0d0121; color: white;">
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
@@ -57,13 +57,33 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Повторите пароль') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control neon blue" name="password_confirmation" required style="background-color: #0d0121; color: white;">
                             </div>
                         </div>
 
                         <div class="form-group row mb-3">
                             <div class="col-md-6 offset-md-4">
-                                <a href="/faq">Пользовательское соглашение</a>
+                                <a href="#" data-toggle="modal" data-target="#exampleModalLong">Пользовательское соглашение</a>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                  <div class="modal-dialog" role="document">
+                                    <div class="modal-content intro neon blue" style="padding-bottom: 0px; padding-top: 0px;">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Правила</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                        </button>
+                                      </div>
+                                      <div class="modal-body">
+                                        ...
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary neon gren" data-dismiss="modal" style="background-color: #02fd1e80; border: none;">Закрыть</button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
                                 <br>
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                 <label class="form-check-label" for="exampleCheck1">Я согласен</label>
@@ -72,7 +92,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary neon gren" style="background-color: #02fd1e80; border: none;">
                                     {{ __('Зарегисрироваться') }}
                                 </button>
                             </div>
